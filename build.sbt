871b7fd4b635f979cab1e84018a82016ab435107
Hisao Soyama <hisao.soyama@gmail.com>
2016-12-30 15:14:06 +0900

    Refactor Perceptron and add test

--- build.sbt
+++ build.sbt
@@ -6,7 +6,9 @@ scalaVersion := "2.11.8"
 
 libraryDependencies  ++= Seq(
   "org.scalanlp" %% "breeze" % "0.12",
-  "org.scalanlp" %% "breeze-natives" % "0.12"
+  "org.scalanlp" %% "breeze-natives" % "0.12",
+  "org.scalactic" %% "scalactic" % "3.0.1",
+  "org.scalatest" %% "scalatest" % "3.0.1" % "test"
 )
 
 resolvers += "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/"
--- src/main/scala/Perceptron.scala
+++ src/main/scala/Perceptron.scala
@@ -1,30 +1,16 @@
 import breeze.linalg._
 
 object Perceptron {
-  def gate(w: DenseVector[Double], b: Double)(x1: Double, x2: Double): Double = {
+  private def gate(w: DenseVector[Double], b: Double)(x1: Double, x2: Double): Double = {
     assert(w.size == 2)
     val x = DenseVector(x1, x2)
     if ((w dot x) + b <= 0) 0.0
     else 1.0
   }
 
-  def AND(x1: Double, x2: Double): Double = {
-    val w = DenseVector(0.5, 0.5)
-    val b = -0.7
-    gate(w, b)(x1, x2)
-  }
-
-  def NAND(x1: Double, x2: Double): Double = {
-    val w = DenseVector(-0.5, -0.5)
-    val b = 0.7
-    gate(w, b)(x1, x2)
-  }
-
-  def OR(x1: Double, x2: Double): Double = {
-    val w = DenseVector(0.5, 0.5)
-    val b = -0.2
-    gate(w, b)(x1, x2)
-  }
+  val AND = gate(DenseVector(0.5, 0.5), -0.7)_
+  val NAND = gate(DenseVector(-0.5, -0.5), 0.7)_
+  val OR = gate(DenseVector(0.5, 0.5), -0.2)_
 
   def XOR(x1: Double, x2: Double): Double = {
     val s1 = NAND(x1, x2)
