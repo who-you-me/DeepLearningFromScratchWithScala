50e33c1b510597445bd23e63f4da185614fab703
Hisao Soyama <hisao.soyama@gmail.com>
2017-01-02 13:00:27 +0900

    Cache dataset object

--- src/main/scala/Mnist.scala
+++ src/main/scala/Mnist.scala
@@ -6,44 +6,71 @@ import java.util.zip.GZIPInputStream
 import scala.collection.mutable.ListBuffer
 import breeze.linalg.{DenseMatrix, DenseVector}
 
-class Mnist(prefix: String) {
-  assert(Set("train", "test").contains(prefix))
-  val filePrefix = if (prefix == "test") "t10k" else prefix
+object Mnist {
+  val dataSetDir = List(System.getenv("HOME"), ".cache", "mnist").mkString(File.separator)
+  private val path = List(dataSetDir, "mnist.ser").mkString(File.separator)
 
-  private val imageLoader = new ImageLoader(s"$filePrefix-images-idx3-ubyte.gz")
-  private val labelLoader = new LabelLoader(s"$filePrefix-labels-idx1-ubyte.gz")
-
-  private def load(normalize: Boolean, oneHotLabel: Boolean) = {
-    val images = imageLoader.loadAsMatrix(normalize)
-    val labels = labelLoader.load(oneHotLabel)
-    (images, labels)
-  }
-
-  private def loadFlatten(normalize: Boolean, oneHotLabel: Boolean) = {
-    val images = imageLoader.loadAsVector(normalize)
-    val labels = labelLoader.load(oneHotLabel)
-    (images, labels)
+  def get() = {
+    if (Files.exists(Paths.get(path))) {
+      val ois = new ObjectInputStream(new FileInputStream(path))
+      try
+        ois.readObject().asInstanceOf[Mnist]
+      finally
+        ois.close()
+    } else {
+      val mnist = new Mnist
+      val oos = new ObjectOutputStream(new FileOutputStream(path))
+      try
+        oos.writeObject(mnist)
+      finally
+        oos.close()
+      mnist
+    }
   }
 }
 
-object Mnist {
-  def load(normalize: Boolean = true, oneHotLabel: Boolean = false) = {
-    val train = new Mnist("train").load(normalize, oneHotLabel)
-    val test = new Mnist("test").load(normalize, oneHotLabel)
-    (train, test)
+class Mnist extends Serializable {
+  val trainImg = new ImageData("train")
+  val trainLabel = new LabelData("train")
+  val testImg = new ImageData("test")
+  val testLabel = new LabelData("test")
+}
+
+class Data(prefix: String) extends Serializable {
+  assert(Set("train", "test").contains(prefix))
+  val filePrefix = if (prefix == "test") "t10k" else prefix
+}
+
+class ImageData(prefix: String) extends Data(prefix) {
+  private val data = new ImageLoader(s"$filePrefix-images-idx3-ubyte.gz").load()
+
+  def get(normalize: Boolean = true): List[DenseMatrix[Double]] =
+    if (normalize) normalizeFunc(data)
+    else data
+
+  def getFlatten(normalize: Boolean = true): List[DenseVector[Double]] = {
+    flattenFunc(get(normalize))
   }
 
-  def loadFlatten(normalize: Boolean = true, oneHotLabel: Boolean = false) = {
-    val train = new Mnist("train").loadFlatten(normalize, oneHotLabel)
-    val test = new Mnist("test").loadFlatten(normalize, oneHotLabel)
-    (train, test)
-  }
+  private def normalizeFunc(xs: List[DenseMatrix[Double]]): List[DenseMatrix[Double]] =
+    xs.map(_ / 255.0)
+
+  private def flattenFunc(xs: List[DenseMatrix[Double]]): List[DenseVector[Double]] =
+    xs.map(_.t.toDenseVector)
+}
+
+class LabelData(prefix: String) extends Data(prefix) {
+  private val data = new LabelLoader(s"$filePrefix-labels-idx1-ubyte.gz").load()
+
+  def get(): List[Int] = data
+
+  //  def getOneHot(): List[List[Int]]
 }
 
 class Loader(fileName: String) {
   val urlBase = "http://yann.lecun.com/exdb/mnist/"
+  val dataSetDir = List(System.getenv("HOME"), ".cache", "mnist").mkString(File.separator)
 
-  private val dataSetDir = List(System.getenv("HOME"), ".cache", "mnist").mkString(File.separator)
   private val dir = new File(dataSetDir)
   if (!dir.exists) dir.mkdirs()
 
@@ -74,42 +101,25 @@ class ImageLoader(fileName: String) extends Loader(fileName) {
   val rows = stream.readInt()
   val cols = stream.readInt()
 
-  def loadAsMatrix(normalize: Boolean): List[DenseMatrix[Double]] = {
+  def load(): List[DenseMatrix[Double]] = {
     print("Converting " + fileName + " to DenseMatrix ...")
 
     val buf = new ListBuffer[DenseMatrix[Double]]
     for (_ <- 0 until count) {
-      buf += loadOneAsMatrix(normalize)
+      buf += loadOne()
     }
 
     println("Done")
     buf.toList
   }
 
-  def loadAsVector(normalize: Boolean): List[DenseVector[Double]] = {
-    print("Converting " + fileName + " to DenseVector ...")
-
-    val buf = new ListBuffer[DenseVector[Double]]
-    for (_ <- 0 until count) buf += loadOneAsVector(normalize)
-
-    println("Done")
-    buf.toList
-  }
-
-  private def loadOneAsMatrix(normalize: Boolean): DenseMatrix[Double] = {
+  private def loadOne(): DenseMatrix[Double] = {
     val matrix = DenseMatrix.zeros[Double](rows, cols)
     for {y <- 0 until cols
          x <- 0 until rows} {
-      val value: Double = stream.readUnsignedByte()
-      if (normalize) matrix(y, x) = value / 255.0
-      else matrix(y, x) = value
+      matrix(y, x) = stream.readUnsignedByte()
     }
     matrix
-  }
-
-  private def loadOneAsVector(normalize: Boolean): DenseVector[Double] = {
-    val matrix = loadOneAsMatrix(normalize)
-    matrix.t.toDenseVector
   }
 }
 
@@ -119,9 +129,7 @@ class LabelLoader(fileName: String) extends Loader(fileName) {
 
   val count = stream.readInt()
 
-  def load(oneHotLabel: Boolean): List[Int] = {
-    // TODO: oneHotLabel
-
+  def load(): List[Int] = {
     print("Converting " + fileName + " to List ...")
     val buf = new ListBuffer[Int]
     for (_ <- 0 until count) buf += stream.readUnsignedByte()
@@ -130,4 +138,3 @@ class LabelLoader(fileName: String) extends Loader(fileName) {
     buf.toList
   }
 }
-
